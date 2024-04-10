4ae5d4b5b5ce2dbbb222575c15377a2b75c3198d
deepak1051 <lightyagamigoku@gmail.com>
2024-04-10 23:08:27 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,1 +1,9 @@
-console.log('welcome to ebook apis');
+import app from "./src/app";
+import { config } from "./src/config/config";
+
+const startServer = () => {
+  const port = config.port || 3000;
+  app.listen(port, () => console.log(`server is running at ${port}`));
+};
+
+startServer();
