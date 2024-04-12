eafb61ebf43d07e37e7a53c6431a35b783b70ed7
deepak1051 <lightyagamigoku@gmail.com>
2024-04-12 10:24:11 +0530

    upload files to cloudinary

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,9 +1,23 @@
+import path from "node:path";
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
 
 const router = express.Router();
 
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 }, //30mb
+});
+
 //routes
-router.post("/", createBook);
+router.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default router;
